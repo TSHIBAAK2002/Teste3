1ab6c03a3ea4729b77d20b82a560b8e0a784df89
TSHIBAAK2002 <167263366+TSHIBAAK2002@users.noreply.github.com>
2024-05-04 23:53:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const nomUser = document.getElementById('nom');
+const prenomUser = document.getElementById('prenom');
+const numeroUser = document.getElementById('numero');
+const prestationUser = document.getElementById('prestation');
+const avenueUser = document.getElementById('avenue');
+
+const soummettre = document.getElementById('bouttonSoumettre').addEventListener('click',function(){
+    
+} );
